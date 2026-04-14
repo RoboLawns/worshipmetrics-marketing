40f71728e56a59c301b094054ef1445e2d3791e4
Paul Richards <paul.w.richards87@gmail.com>
2026-04-14 13:01:07 -0400

    chore: enable cloudflare server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
@@ -9,6 +10,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://worshipmetrics.com',
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()]
